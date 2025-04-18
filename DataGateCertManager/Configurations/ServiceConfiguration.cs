a78f618e46fcb1730195e224df5f14921be7236b
Ivan Kolganov <imkolganov@gmail.com>
2025-04-18 16:16:38 +0300

    Registers core and EasyRsa services.
    Configures dependency injection for core services
    related to certificate and VPN management, and
    EasyRsa services for key generation and database
    interaction.

--- DataGateCertManager/Configurations/ServiceConfiguration.cs
+++ DataGateCertManager/Configurations/ServiceConfiguration.cs
@@ -1,10 +1,24 @@
 ﻿
+using DataGateCertManager.Services;
+using DataGateCertManager.Services.EasyRsaServices;
+using DataGateCertManager.Services.EasyRsaServices.Interfaces;
+using DataGateCertManager.Services.Interfaces;
+
 namespace DataGateCertManager.Configurations;
 
 public static class ServiceConfiguration
 {
     public static void ConfigureServices(this IServiceCollection services)
     {
+        // Core services
+        services.AddScoped<ICertVpnService, CertVpnService>();
+        
+        // EasyRsa services
+        services.AddScoped<IEasyRsaService, EasyRsaService>();
+        services.AddScoped<IEasyRsaParseDbService, EasyRsaParseDbService>();
+        services.AddScoped<IEasyRsaExecCommandService, EasyRsaExecCommandService>();
+        services.AddScoped<IBashCommandRunner, BashCommandRunner>();
+        
         services.AddControllers();
 
         services.AddEndpointsApiExplorer();
